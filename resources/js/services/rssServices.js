e4fcda2421812d90575b5a31183c1fdbaa795d86
Kazem Mirzaei <k90mirzaei@gmail.com>
2022-07-31 18:25:56 +0430

    resolve some issue

--- resources/js/services/rssServices.js
+++ resources/js/services/rssServices.js
@@ -4,8 +4,6 @@ const RSS_KEY = process.env.MIX_RSS_API_KEY;
 const RSS_URL = process.env.MIX_RSS_URL;
 const RSS_API = process.env.MIX_RSS_API
 
-console.log(process.env)
-
 const fetchRss = (count = null) => {
     return api.get(RSS_API, {
         params: {
