b70d5880b5727b9a952159e0772f90d4200d858d
Kazem Mirzaei <k90mirzaei@gmail.com>
2022-07-31 15:33:05 +0430

    add resource for vue2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,16 @@
-import { createApp } from 'vue'
+import "./bootstrap"
+
+import Vue from 'vue'
+import VueRouter from "vue-router"
+import router from './router/routes'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
+window.Vue = Vue;
 
-app.use(router)
-app.mount('#app')
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    render: h => h(App),
+}).$mount('#app')
